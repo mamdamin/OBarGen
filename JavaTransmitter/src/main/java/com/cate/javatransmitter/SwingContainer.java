a339de5c9ac6c8f635ceacb962fe302098486d5a
mamdamin <aminmotahari@yahoo.com>
2015-02-15 18:04:14 -0500

    Removed URL loading. 100 frames in 780ms

--- JavaTransmitter/src/main/java/com/cate/javatransmitter/SwingContainer.java
+++ JavaTransmitter/src/main/java/com/cate/javatransmitter/SwingContainer.java
@@ -1,8 +1,27 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2015 Amin Motahari.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
+
 package com.cate.javatransmitter;
 
 import java.awt.*;
@@ -80,24 +99,24 @@ public class SwingContainer {
 
 
 public void showBarcode(BufferedImage barcode){
-    BufferedImage img=null;
-    URL google = null;
-      try {
-             google = new URL("http://www.google.com/images/srpr/logo11w.png");
-        } 
-      catch (IOException e) {
-    // exception handler code here
-    // ...
-        }
-   
-      try{
-          
-        img = ImageIO.read(google);
-      
-      }
-      catch (IOException e) {
-          
-      }
+//    BufferedImage img=null;
+//    URL google = null;
+//      try {
+//             google = new URL("http://www.google.com/images/srpr/logo11w.png");
+//        } 
+//      catch (IOException e) {
+//    // exception handler code here
+//    // ...
+//        }
+//   
+//      try{
+//          
+//        img = ImageIO.read(google);
+//      
+//      }
+//      catch (IOException e) {
+//          
+//      }
    
 //      int w = img.getWidth(null);
 //      int h = img.getHeight(null);
@@ -117,7 +136,7 @@ public void showBarcode(BufferedImage barcode){
 
 
 
-      Timer timer;
+//      Timer timer;
 //      for (int i=0;i<100;i++){
 //        picLabel.setIcon(new ImageIcon(barcode));
 //          try {
